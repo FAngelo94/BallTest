82cf4ee7f635ee43dfc9d768dee72993ccff5cf0
unknown <a.falci@live.it>
2019-01-12 18:14:59 +0100

    ball ready

--- Assets/Scripts/ManageMove.cs
+++ Assets/Scripts/ManageMove.cs
@@ -5,28 +5,47 @@ using UnityEngine;
 public class ManageMove : MonoBehaviour
 {
     public float jumpForce = 10;
-    public float speed = 20;
+    public float speed = 8;
 
+    private int directionX;
+    private bool canJump;
     private Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        directionX = 0;
+        canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        int directionX = 0;
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+        
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && canJump)
         {
+            Debug.Log("Jump");
             body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            canJump = false;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        directionX = 0;
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Right");
             directionX = 1;
-        if (Input.GetKeyDown(KeyCode.A))
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Left");
             directionX = -1;
-        body.velocity = new Vector2(speed * directionX, 0);
+        }
+        
+        body.velocity = new Vector2(speed * directionX, body.velocity.y);
 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("CollisionEnter2D");
+        canJump=true;
     }
 }
