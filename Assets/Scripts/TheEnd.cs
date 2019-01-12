612f56c22aa5638d6be979a0e36e37f8bcfff2a4
unknown <a.falci@live.it>
2019-01-12 19:49:27 +0100

    level the end added

---
+++ Assets/Scripts/TheEnd.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheEnd : MonoBehaviour
+{
+
+    public GameObject endWrite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("End");
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        endWrite.SetActive(true);
+    }
+}
